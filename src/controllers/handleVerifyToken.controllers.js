3bff22e6eed7d9afae2d52bd76ca55cb8733540a
codeshaine <shainilps05@gmail.com>
2024-04-18 22:28:30 +0530

    debugging cookie error

--- src/controllers/handleVerifyToken.controllers.js
+++ src/controllers/handleVerifyToken.controllers.js
@@ -4,7 +4,7 @@ import { verifyToken } from "../utils/authentication.js";
 import { User } from "../models/user.model.js";
 const handleVerifyToken = asyncHandler(async (req, res) => {
   let token = req.cookies.accessToken;
-  console.log(token);
+  console.log(req.cookies);
   const isTokenValid = verifyToken(token);
 
   //sending user data to client
